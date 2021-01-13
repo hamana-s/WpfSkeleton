070799f5cb2166907aa160009eb2ef61cab4fe91
sakurai_no <sakurai_no@mti.co.jp>
2021-01-13 18:51:39 +0900

    テストを1つにまとめる

--- UnitTest/Tests/CalculationTest.cs
+++ UnitTest/Tests/CalculationTest.cs
@@ -29,16 +29,6 @@ namespace UnitTest.Tests
 
             "値の計算1"
                 .x(() => { actual = Calculation.Sum(10, 20); Assert.Equal(result, actual); });
-        }
-
-        [Scenario]
-        public void CalcTest2()
-        {
-            int result = 0;
-            int actual;
-
-            "値初期化"
-                .x(() => { result = 30; });
 
             "値の計算2"
                 .x(() => { actual = Calculation.Sum(20, 20); Assert.Equal(result, actual); });
