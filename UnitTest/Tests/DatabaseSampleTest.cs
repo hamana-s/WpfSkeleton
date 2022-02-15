8818902db7967596d758bd89ec899bef57e19a89
hamana <hamana_s@mti.co.jp>
2022-02-15 17:21:39 +0900

    Xbehave削除

--- UnitTest/Tests/DatabaseSampleTest.cs
+++ UnitTest/Tests/DatabaseSampleTest.cs
@@ -2,31 +2,33 @@
 using Microsoft.EntityFrameworkCore;
 using Models.DB.Tables;
 using UnitTest.Infrastructures;
-using Xbehave;
+using Xunit;
 
 namespace UnitTest.Tests
 {
     public class DatabaseSampleTest : DBTestBase
     {
-        [Scenario]
-        public void GetSamplesTest()
+        [Fact]
+        public async void GetSamplesTest()
         {
-            "テーブルが空の際は空のデータが返却されること"
-                .x(async () => (await dataContext.Samples.ToArrayAsync()).Should().BeEmpty());
+            (await dataContext.Samples.ToArrayAsync()).Should().BeEmpty();
+        }
 
-            "レコードの追加に成功すること"
-                .x(async () =>
-                {
-                    await dataContext.Samples.AddRangeAsync(new Sample[]{
-                        new (){ Data = "データ1" },
-                        new (){ Data = "データ2" },
-                    });
-                    await dataContext.SaveChangesAsync();
-                    (await dataContext.Samples.ToArrayAsync()).Should().BeEquivalentTo(new Sample[]{
-                        new (){ ID = 1, Data = "データ1" },
-                        new (){ ID = 2, Data = "データ2" },
-                    });
-                });
+        [Fact]
+        public async void GetSamplesTest_Insert()
+        {
+            await dataContext.Samples.AddRangeAsync(new Sample[]
+            {
+                new (){ Data = "データ1" },
+                new (){ Data = "データ2" },
+            });
+            await dataContext.SaveChangesAsync();
+
+            (await dataContext.Samples.ToArrayAsync()).Should().BeEquivalentTo(new Sample[]
+            {
+                new (){ ID = 1, Data = "データ1" },
+                new (){ ID = 2, Data = "データ2" },
+            });
         }
     }
 }
